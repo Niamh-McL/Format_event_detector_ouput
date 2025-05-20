1f33ce692a3a0bf25e32daae5d942d4b48f1ef0c
paulrignanese <paul.rignanese@gmail.com>
2025-05-20 10:06:05 +0100

    rename main script to main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  2 14:22:03 2024
+
+@author: niamh
+"""
+
+import numpy as np 
+import pandas as pd
+
+#only selecting events in the 24 hours of interest
+rat = 381
+
+sample_rate = 250.4
+
+startIndex = 12214513
+endIndex = 33849072
+
+startSec = startIndex/sample_rate
+endSec = endIndex/sample_rate
+
+print(startSec)
+print(endSec)
+
+"To load the data, put file location and name below using double back to front slash"
+SWD_events_path="/home/paul/Desktop/scientific_programming/csv_example/events_SCN2A_381.csv"
+
+# Load SWD events from .csv
+SWD_events = pd.read_csv(SWD_events_path, delimiter=",", header=None)
+
+SWD_events.rename(columns={0: 'start_sec', 1:'end_sec'}, inplace=True)
+
+SWD_events['duration'] = SWD_events['end_sec'] - SWD_events['start_sec']
+SWD_events = SWD_events.round(1)
+
+SWD_events_BL1 = []
+for index in range(len(SWD_events)):
+    if (SWD_events.iloc[index, 0] > startSec) &  (SWD_events.iloc[index, 1] < endSec):
+        SWD_events_BL1.append(SWD_events.iloc[index, :])
+
+SWD_events_BL1 = pd.DataFrame(SWD_events_BL1)
+
+SWD_events_BL1['start_sec'] -= startSec
+SWD_events_BL1['end_sec'] -= startSec
+
+SWD_events_BL1_round = SWD_events_BL1.round(1)
+
+save_folder = "/home/paul/Desktop/scientific_programming/csv_example/"
+save_path = f'{save_folder}/{rat}_24h_reformatted_file.csv'
+SWD_events_BL1_round .to_csv(save_path, index=False)
+
